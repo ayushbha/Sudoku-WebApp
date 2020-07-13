f48e958b28678943c9ea9a89831a6faa008d3720
Ayush Bharat Jain <60114945+ayushbha@users.noreply.github.com>
2020-07-13 01:33:21 -0700

    Updates

---
+++ precache-manifest.1a50a6b9b2e1acb7024f0d1608261fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9b62d06fb20e1686e0b1f4770fbe35",
+    "url": "/Sudoku-WebApp/index.html"
+  },
+  {
+    "revision": "c6d53530b1a3e000e12c",
+    "url": "/Sudoku-WebApp/static/css/main.f8bff51d.chunk.css"
+  },
+  {
+    "revision": "e8a5377890f92f12579e",
+    "url": "/Sudoku-WebApp/static/js/2.18877ab8.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/Sudoku-WebApp/static/js/2.18877ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d53530b1a3e000e12c",
+    "url": "/Sudoku-WebApp/static/js/main.ab8bdc2a.chunk.js"
+  },
+  {
+    "revision": "6d825a19ed289dc3277a",
+    "url": "/Sudoku-WebApp/static/js/runtime-main.8c1f33ba.js"
+  }
+]);
